fc41f02c28c941477b57f73764ee66a4c26737eb
Jonghyun <jonghyun.yoo0323@gmail.com>
2021-01-20 15:32:10 +0900

    added new comments and String array just for fun..

--- src/org/comstudy/HelloGitWorld.java
+++ src/org/comstudy/HelloGitWorld.java
@@ -5,5 +5,15 @@ public class HelloGitWorld {
 		String user = "John Yoo";
 		
 		System.out.println("User =>"+user);
+		
+		//introducing git hub to my programming record!
+		//dont know if it works correctly or not.
+		// we will see
+		
+		String[] str = new String[10];
+		for(int i = 0; i<10;i++) {
+			String a = ""+i;
+			str[i] = a;
+		}
 	}
 }
